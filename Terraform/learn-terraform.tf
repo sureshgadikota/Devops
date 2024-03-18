cb83fbe89f646030c61710912a3efbb847aa6604
Suresh <sureshgadikota@gmail.com>
2024-03-18 17:47:47 +0530

    code updated

--- Terraform/learn-terraform.tf
+++ Terraform/learn-terraform.tf
@@ -2,12 +2,16 @@ variable "ami" {
   default = "ami-0f3c7d07486cad139"
 }
 
+variable "security_groups" {
+  default = ["sg-006a2998b3aed70ca"]
+}
+
 variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "security_groups" {
-  default = ["sg-006a2998b3aed70ca"]
+variable "zone_id" {
+  default = "Z049869630CEPSXALO943"
 }
 
 variable "components" {
@@ -24,25 +28,24 @@ variable "components" {
     rabbitmq  = { name = "rabbitmq-dev" }
   }
 }
+
 resource "aws_instance" "instance" {
-  for_each      = var.components
-  ami           = var.ami
-  instance_type = var.instance_type
+  for_each               = var.components
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = var.security_groups
 
-
-
   tags = {
-    name     = lookup(each.value, "name", null)
+    Name = lookup(each.value, "name", null)
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z049869630CEPSXALO943"
-  name    = "frontend-dev.devopskumar.site"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend-dev.private_ip]
 
-
-
+resource "aws_route53_record" "record" {
+  for_each = var.components
+  zone_id  = var.zone_id
+  name     = "${lookup(each.value, "name", null)}.rdevopsb72.online"
+  type     = "A"
+  ttl      = 30
+  records  = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
+}
