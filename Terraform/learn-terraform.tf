27b58503227981068005e550563c97bf8797e7fd
Suresh <sureshgadikota@gmail.com>
2024-03-18 17:33:32 +0530

    khasks

--- Terraform/learn-terraform.tf
+++ Terraform/learn-terraform.tf
@@ -1,11 +1,19 @@
 variable "components" {
   default = {
-    frontend  = { name = "frontend" }
-    catalogue = { name = "catalogue" }
-    mongodb   = { name = "mongodb" }
+    frontend  = { name = "frontend-dev" }
+    catalogue = { name = "catalogue-dev" }
+    mongodb   = { name = "mongodb-dev" }
+    user      = { name = "user-dev" }
+    redis     = { name = "redis-dev" }
+    cart      = { name = "cart-dev" }
+    mysql     = { name = "mysql-dev" }
+    shipping  = { name = "shipping-dev" }
+    payment   = { name = "payment-dev" }
+    rabbitmq  = { name = "rabbitmq-dev" }
   }
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "instance" {
+  for_each               = var.components
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-006a2998b3aed70ca"]
@@ -13,8 +21,7 @@ resource "aws_instance" "web" {
 
 
   tags = {
-
-    name     = lookup(var.components, each.value["name"], null)
+    name     = lookup(each.value, "name", null)
   }
 }
 
