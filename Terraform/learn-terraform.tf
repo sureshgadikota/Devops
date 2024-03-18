559738b8a8293b9b4879d318e5be843c29b40a93
Suresh <sureshgadikota@gmail.com>
2024-03-18 16:46:45 +0530

    khasks

--- Terraform/learn-terraform.tf
+++ Terraform/learn-terraform.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-006a2998b3aed70ca"]
 
-  security_groups = "sg-006a2998b3aed70ca"
+
+
+
+
 
   tags = {
     Name = "HelloWorld"
